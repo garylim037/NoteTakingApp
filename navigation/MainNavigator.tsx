ea6e7adaee02b17f42cbb26a9a1b714a96813e5f
LOW JUN XIANG <junxianglow88@1utar.my>
2026-05-03 23:41:39 +0800

    Update MainNavigator.tsx

--- navigation/MainNavigator.tsx
+++ navigation/MainNavigator.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import HomeScreen from '../screens/HomeScreen';
 import AddNoteScreen from '../screens/AddNoteScreen';
 import NoteDetailsScreen from '../screens/NoteDetailsScreen';
